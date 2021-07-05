5cb185f1494c5167be9d0d9c8fc2f6591be9eb67
Ly Thanh Dat <dat6499proo@gmail.com>
2021-07-05 20:19:54 +0700

    fix

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
 app.set("port", 3843);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors())
 const searcher = require("./mappings/searcher");
 
-app.get("/person/search", async (req, res) => {
+app.post("/person/search", async (req, res) => {
   const data = await searcher.person(req.body.keywords, req.body.size || 10);
   res.json(data);
 });
 
-app.get("/flim/search", async (req, res) => {
+app.post("/flim/search", async (req, res) => {
   const data = await searcher.flim(req.body.keywords, req.body.size || 10);
   res.json(data);
 });
 
-app.get("/post/search", async (req, res) => {
+app.post("/post/search", async (req, res) => {
   const data = await searcher.post(req.body.keywords, req.body.size || 10);
   res.json(data);
 });
 
-app.get("/all/search", async (req, res) => {
+app.post("/all/search", async (req, res) => {
   const person = await searcher.person(req.body.keywords, req.body.size || 10);
   const flim = await searcher.flim(req.body.keywords, req.body.size || 10);
   const post = await searcher.post(req.body.keywords, req.body.size || 10);
